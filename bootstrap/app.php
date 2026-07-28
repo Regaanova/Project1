7f3fab34600455c82c21a7abc1a8a3fd3344e547
Rega <reganova21@example.com>
2026-07-28 21:52:03 +0700

    fix error message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Helpers\ResponseHelper;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,9 +9,9 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,6 +19,18 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn(Request $request) => $request->is('api/*'),
+        );
+
+        $exceptions->render(
+            function (AuthenticationException $exception, Request $request) {
+                if ($request->is('api/*')) {
+                    return ResponseHelper::error(
+                        'Token tidak valid atau Anda belum login.',
+                        null,
+                        401
+                    );
+                }
+            }
         );
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+
+    Route::get('/user', function () {
+        return auth()->user();
     });
 });
